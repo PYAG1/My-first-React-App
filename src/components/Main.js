a49d10a6ad279107afe973a1daeae7369f7371fd
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2022-10-29 16:06:53 +0100

    commit 1

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+
+export default function Main(){
+    return(
+        <h1>paps</h1>
+    )
+};
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import reactlogo from "../images/logo192.png"
+
+
+
+export default function Navbar(){
+    return(
+        <div>
+            <img src={reactlogo}></img>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </div>
+    )
+};
+
