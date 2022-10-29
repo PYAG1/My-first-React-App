e2c2d425407a462e4630638fe3ba91d14f4cea9b
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2022-10-29 17:21:16 +0100

    commit 2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import halflogo from "../images/reactjs-icon 2@2x.png"
 
 
 
 export default function Main(){
     return(
-        <h1>paps</h1>
+        <div className='Main'>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+
+        </div>
     )
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ import reactlogo from "../images/logo192.png"
 
 export default function Navbar(){
     return(
-        <div>
+        <nav>
             <img src={reactlogo}></img>
             <h3>ReactFacts</h3>
             <h4>React Course - Project 1</h4>
-        </div>
+        </nav>
     )
 };
 
